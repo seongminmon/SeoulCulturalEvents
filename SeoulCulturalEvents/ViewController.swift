9defc336bdd3ded167036a386f5eb3729c384ec2
seongmin kim <k2417000@naver.com>
2024-08-14 15:01:00 +0900

    Initial Commit

---
+++ SeoulCulturalEvents/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeoulCulturalEvents
+//
+//  Created by 김성민 on 8/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
