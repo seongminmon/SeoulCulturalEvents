fbf48983d0b8991d4fcd485449cd4c0782148bd8
seongmin kim <k2417000@naver.com>
2024-08-14 21:35:51 +0900

    커스텀뷰 생성

--- SeoulCulturalEvents/Source/CustomView/PointButton.swift
+++ SeoulCulturalEvents/Source/CustomView/PointButton.swift
@@ -5,4 +5,21 @@
 //  Created by 김성민 on 8/14/24.
 //
 
-import Foundation
+import UIKit
+
+final class PointButton: UIButton {
+    
+    init(title: String) {
+        super.init(frame: .zero)
+        setTitle(title, for: .normal)
+        titleLabel?.font = .bold17
+        setTitleColor(.white, for: .normal)
+        backgroundColor = .systemPink
+        layer.cornerRadius = 10
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- SeoulCulturalEvents/Source/CustomView/SignTextField.swift
+++ SeoulCulturalEvents/Source/CustomView/SignTextField.swift
@@ -5,4 +5,23 @@
 //  Created by 김성민 on 8/14/24.
 //
 
-import Foundation
+import UIKit
+
+final class SignTextField: UITextField {
+    
+    init(placeholderText: String) {
+        super.init(frame: .zero)
+        textColor = .black
+        placeholder = placeholderText
+        textAlignment = .center
+        borderStyle = .none
+        layer.cornerRadius = 10
+        layer.borderWidth = 1
+        layer.borderColor = UIColor.black.cgColor
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
