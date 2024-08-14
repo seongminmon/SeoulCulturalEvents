b0d5fd18ae4b601574737124b740b832cb838c55
seongmin kim <k2417000@naver.com>
2024-08-14 20:04:41 +0900

    DateFormatting 기능 구현

--- SeoulCulturalEvents/Resource/Extension/Date+.swift
+++ SeoulCulturalEvents/Resource/Extension/Date+.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+extension Date {
+    func toString(_ dateFormat: String = "yyyy-MM-dd") -> String {
+        Formatter.dateFormatter.locale = Locale(identifier: "ko_KR")
+        Formatter.dateFormatter.timeZone = TimeZone(abbreviation: "KST")
+        Formatter.dateFormatter.dateFormat = dateFormat
+        return Formatter.dateFormatter.string(from: self)
+    }
+}
--- SeoulCulturalEvents/Resource/Extension/String+.swift
+++ SeoulCulturalEvents/Resource/Extension/String+.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+extension String {
+    func toDate(_ dateFormat: String = "yyyy-MM-dd HH:mm:ss.S") -> Date? {
+        Formatter.dateFormatter.locale = Locale(identifier: "ko_KR")
+        Formatter.dateFormatter.timeZone = TimeZone(identifier: "KST")
+        Formatter.dateFormatter.dateFormat = dateFormat
+        return Formatter.dateFormatter.date(from: self)
+    }
+}
