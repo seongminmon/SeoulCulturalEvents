b4f6507b1065c7d94b051e19222697e144daac3e
seongmin kim <k2417000@naver.com>
2024-08-14 22:23:23 +0900

    Moya 설치, 문화 행사 API 구성하기

---
+++ SeoulCulturalEvents/Source/Network/CultureRouter.swift
@@ -0,0 +1,60 @@
+//
+//  CultureRouter.swift
+//  SeoulCulturalEvents
+//
+//  Created by 김성민 on 8/14/24.
+//
+
+import Foundation
+import Moya
+
+struct CultureParameter {
+    let startDate: Int
+    let endDate: Int
+    let codeName: CodeName?
+    let title: String?
+    let date: Date?
+}
+
+enum CultureRouter {
+    case cultures(_ parameter: CultureParameter)
+}
+
+extension CultureRouter: TargetType {
+    var baseURL: URL {
+        return URL(string: APIURL.cultureURL)!
+    }
+    
+    var path: String {
+        switch self {
+        case .cultures(let parameter):
+            let startDate = "\(parameter.startDate)"
+            let endDate = "\(parameter.endDate)"
+            let codeName = "\(parameter.codeName?.rawValue ?? "%20")"
+            let title = "\(parameter.title ?? "%20")"
+            let dateStr = parameter.date?.toString() ?? "%20"
+            return "\(startDate)/\(endDate)/\(codeName)/\(title)/\(dateStr)"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .cultures(_):
+            return .get
+        }
+    }
+    
+    var task: Task {
+        switch self {
+        case .cultures(_):
+            return .requestPlain
+        }
+    }
+    
+    var headers: [String : String]? {
+        switch self {
+        case .cultures(_):
+            return nil
+        }
+    }
+}
