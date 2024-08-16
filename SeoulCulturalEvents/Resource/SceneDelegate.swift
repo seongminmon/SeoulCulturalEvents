09b845528a681039ac0e71df892d4f4ec9374bbf
seongmin kim <k2417000@naver.com>
2024-08-16 11:51:36 +0900

    로그인 여부에 따른 첫 화면 분기처리

--- SeoulCulturalEvents/Resource/SceneDelegate.swift
+++ SeoulCulturalEvents/Resource/SceneDelegate.swift
@@ -6,22 +6,35 @@
 //
 
 import UIKit
+import RxSwift
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    
+    let disposeBag = DisposeBag()
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         
-        let vc = SignInViewController()
-        let nav = UINavigationController(rootViewController: vc)
-        window?.rootViewController = nav
-        
-//        let tab = TabBarController()
-//        window?.rootViewController = tab
-        
-        window?.makeKeyAndVisible()
+        LSLPAPIManager.shared.callRequest(api: .refresh, model: RefreshModel.self)
+            .subscribe(with: self) { owner, result in
+                switch result {
+                case .success(let data):
+                    print("토큰 갱신 성공 (리프레시 토큰 유효)")
+                    let tab = TabBarController()
+                    owner.window?.rootViewController = tab
+                    owner.window?.makeKeyAndVisible()
+                    
+                case .failure(let error):
+                    print("토큰 갱신 실패 (리프레시 토큰 없음 or 만료")
+                    let vc = SignInViewController()
+                    let nav = UINavigationController(rootViewController: vc)
+                    owner.window?.rootViewController = nav
+                    owner.window?.makeKeyAndVisible()
+                }
+            }
+            .disposed(by: disposeBag)
     }
 }
