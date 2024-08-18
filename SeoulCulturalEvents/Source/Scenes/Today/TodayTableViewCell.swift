8ee4a7e274775dd82b4276418a294fcccc2f88a3
seongmin kim <k2417000@naver.com>
2024-08-18 21:45:47 +0900

    오늘의 행사 셀 화면 구성

---
+++ SeoulCulturalEvents/Source/Scenes/Today/TodayTableViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  TodayTableViewCell.swift
+//  SeoulCulturalEvents
+//
+//  Created by 김성민 on 8/18/24.
+//
+
+import Foundation
---
+++ SeoulCulturalEvents/Source/Scenes/Today/TodayViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  TodayViewModel.swift
+//  SeoulCulturalEvents
+//
+//  Created by 김성민 on 8/18/24.
+//
+
+import Foundation
